f5d5c9f495791fa5b002748c5faf82cd230fe8d7
jinljian <jinljian@cisco.com>
2014-02-27 21:39:02 +0800

    add more code

--- generate_ipv6_link.py
+++ generate_ipv6_link.py
@@ -6,50 +6,59 @@ class IPv6_link(object):
         self.iface =iface
         pass
     @staticmethod
-    def get_mac(iface=None) :
+    def get_iface_mac(iface=None) :
 
         cmd = "/sbin/ifconfig %s |grep HWaddr|awk '{print $5}'"%(iface)
         buf = os.popen(cmd).read()
         mac = buf.replace('\n','').replace(' ','')
 
         return mac
+
     @staticmethod
     def generate_ipv6_link(mac=None):
 
-        # print self.mac
         mac_str = mac.replace(':','')
         if len(mac_str) !=  12 :
             raise Exception("mac address error")
-        # print mac_str
-        # print len(mac_str)
         tmp = mac_str[0:6] +'FFFE' +mac_str[6:]
         mac_str = tmp
-        # print tmp
-        # print len(tmp)
         byte2 = mac_str[1]
-        # print byte2
         byte2 = int(byte2,16)
         byte2 = byte2 ^0x02
         byte2 = '%X'%(byte2)
-        # print byte2
-        # mac_str[1] = byte2[0]
         tmp_str = mac_str[0] + byte2[0] +mac_str[2:]
-        # print tmp_str
-        result =  'FF80' + tmp_str
-        # print result
-        # print len(result)
-        link = result[0:4] + ":" +result[4:8] + ":" +result[8:12] + ":" \
+        result =  'FE80' + tmp_str
+        link = result[0:4] + "::" +result[4:8] + ":" +result[8:12] + ":" \
                +result[12:16] + ":" +result[16:]
         return link
+
+    @staticmethod
+    def get_iface_ipv6_link(iface) :
+        cmd = "/sbin/ifconfig %s |grep Scope:Link | awk '{print $3}'"%(iface)
+        buf = os.popen(cmd).read()
+        if len(buf)  <=  4:
+            raise Exception('no ip link address')
+        link = buf.replace('\n','').replace(' ','')
+        return link.upper()
+    @staticmethod
+    def add_iface_ipv6_link(iface,link_local):
+        cmd  = 'ifconfig %s inet6 add %s/64' %(iface,link_local)
+        os.system(cmd)
+
 
 
 
 if __name__ == "__main__" :
-    print 'I am here'
-    link = IPv6_link("p8p1")
+
+
+
     mac = IPv6_link.get_mac('p8p1')
     print mac
-    print link.generate_ipv6_link(mac)
+    link =  IPv6_link.generate_ipv6_link(mac)
+    print 'Link-->',
+    print link.lower()
+    IPv6_link.add_iface_ipv6_link('p8p1',link)
+
 
 
 
--- get_dns.py
+++ get_dns.py
@@ -1,43 +1,22 @@
 __author__ = 'bluebird'
 import os
+import pprint
 
-def __get_interface_dns(iface):
+def get_all_dns():
         lines = os.popen('nm-tool ').readlines()
-        result = []
-        tmp = {}
-        DNS = []
-        dns_list = {}
+        result = {}
         tmpkey = ''
         for line in lines :
             if line.find("Device:") > 0:
-                dns_list.app
-                keys = lines.split()
-                tmpkey = tmpkey[2]
-                if len(tmp) != 0:
-                    tmp['dns'] = DNS
-                    result.append(tmp)
-                    tmp = {}
-                    DNS = []
-
-                line= line.split()
-                tmp['dev'] = line[2]
+                keys = line.split()
+                tmpdev = keys[2]
+                result[tmpdev]  = []
             else :
                 if line.find('DNS') > 0:
 
                     line = line.split()
-                    DNS.append(line[1])
+                    result[tmpdev].append(line[1])
 
-        tmp['dns']= DNS
-        result.append(tmp)
-    #    pprint.pprint(result)
-        for tmp in result :
-            if tmp['dev'] == iface:
-                return tmp['dns']
-        return []
+        return result
 if __name__ == '__main__' :
-    print __get_interface_dns('p2p1')
-    print "hello"
-    print __get_interface_dns('p7p1')
-    print "hello"
-    print __get_interface_dns('p8p1')
-    print "hello"
+    print get_interface_dns()

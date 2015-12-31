06d00f8736903110a2ce4ca9a55eaad72418e50f
godvmxi <godvmxi@126.com>
2016-01-01 02:27:45 +0800

    load &dump raw

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -7,9 +7,9 @@ import struct
 from  ctypes import *
 import ctypes
 class Header(Structure) :
-    _fields_ = [ ('cmdType',    c_ubyte) ,
-                            ('hash',            c_ubyte) ,
-                            ("length",          c_ubyte ),
+    _fields_ = [            ('cmdType',    ctypes.c_uint8) ,
+                            ('hash',            ctypes.c_uint8) ,
+                            ("length",          ctypes.c_uint8 ),
                             ('counter',       ctypes.c_uint16 ),
                             ('deviceId',      ctypes.c_uint16) ,
                             ('groupId',        ctypes.c_uint16)]
@@ -20,9 +20,27 @@ class Header(Structure) :
         print self.length
         print self.counter
         return "{0}-{1}-{2}-{3}-{4}-{5}".format(  self.cmdType,  self.hash,  self.length,  self.hash,  self.hash,  self.hash)
-    
+    def dumpRaw(self):
+        pass
+    def loadRaw(self,raw):
+        pass
+def struct2stream(s):
+    length = ctypes.sizeof(s)
+    print length
+    p       = ctypes.cast(ctypes.pointer(s), ctypes.POINTER(ctypes.c_char*length))
+    return p.contents.raw
+
+def stream2struct(string, stype):
+    if not issubclass(stype, ctypes.Structure):
+        raise ValueError('The type of the struct is not a ctypes.Structure')
+    length      = ctypes.sizeof(stype)
+    stream      = (ctypes.c_char * length)()
+    stream.raw  = string
+    p           = ctypes.cast(stream, ctypes.POINTER(stype))
+    return p.contents
 if __name__ == "__main__" :
     head = Header();
     head.cmdType=0x11
     head.hash=0xff
     print head
+    print struct2stream(head)

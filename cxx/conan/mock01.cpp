afdd4299a3d12ea6d812ac1001ed15ed2713442a
Jingliang Jiang <jingliang.jiang@continental-corporation.com>
2022-12-28 16:44:37 +0800

    add more lib tester

--- cxx/conan/mock01.cpp
+++ cxx/conan/mock01.cpp
@@ -1,6 +1,12 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <iostream>
+#include <zlib.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/version.hpp>
+
+#include <qrencode.h>
+
 using ::testing::AtLeast;
 using ::testing::NiceMock;
 using ::testing::StrictMock;
@@ -58,8 +64,47 @@ TEST(PainterTest, CanDrawSomething) {
   //EXPECT_TRUE(turtle.PenDown()); 
 }
 
+void lib_tester_zlib()
+{
+    std::cout << "zlib version  : " << zlibVersion() <<std::endl;
+}
+void lib_tester_boost()
+{
+    std::cout << "boost version : " << BOOST_LIB_VERSION << std::endl;
+    std::vector<boost::shared_ptr<int>> v;
+    v.push_back(boost::shared_ptr<int>(new int(1)));
+    v.push_back(boost::shared_ptr<int>(new int(2)));
+    //std::cout << "boost shared_prt  : " << v << std::endl; 
+    std::cout << "boost shared_prt size : " << v.size() << std::endl;
+}
+void lib_tester_qrencode()
+{
+    const char *QRTEXT = "欢迎来到我的的博客";
+    QRcode *qrCode;
+    int version = 5; //设置版本号，这里设为5，对应尺寸：37 * 37
+    QRecLevel level = QR_ECLEVEL_H;
+    QRencodeMode hint = QR_MODE_8;
+    int casesensitive = 1;
+
+    qrCode = QRcode_encodeString(QRTEXT, version, level, hint, casesensitive);
+    if (NULL == qrCode)
+    {
+        std::cerr << "QRcode create fail"<< std::endl;
+        return;
+    }
+    std::cout << "create qr instace ok" << std::endl;
+}
+void lib_tester()
+{
+    lib_tester_zlib();
+    lib_tester_boost();
+    lib_tester_qrencode();
+
+}
+
 int main(int argc, char **argv)
 {
+    lib_tester();
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
